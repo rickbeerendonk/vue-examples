bd56f1205a77326a00b3b4711656e289657587c0
Rick Beerendonk <rick@beerendonk.com>
2020-02-03 17:49:08 +0100

    Add "Vuex - with Vue - Actions - Log" example.

---
+++ Vuex/06 - with Vue - 5. Actions - d. Log/runtime-module/app.js
@@ -0,0 +1,25 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import {
+  mapActions,
+  mapState
+} from '../../../node_modules/vuex/dist/vuex.esm.browser.js';
+
+import { CHANGE, INCREMENT } from './store.js';
+
+export default {
+  name: 'App',
+  computed: mapState(['name', 'year']),
+  methods: mapActions([CHANGE, INCREMENT]),
+  template: `
+    <div>
+      <h1>Hello {{ name }}!</h1>
+      <input :value='name' @input="e => change(e.target.value)" />
+
+      <h1>{{ year }}</h1>
+      <button @click="() => increment()">Increase</button>
+      <button @click="() => increment(5)">Increase with 5</button>
+    </div>
+  `
+};
---
+++ Vuex/06 - with Vue - 5. Actions - d. Log/runtime-module/store.js
@@ -0,0 +1,44 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../../node_modules/vuex/dist/vuex.esm.browser.js';
+
+Vue.use(Vuex);
+
+export const CHANGE = 'change';
+export const INCREMENT = 'increment';
+
+const store = new Vuex.Store({
+  state: {
+    name: 'Svelte',
+    year: 2020
+  },
+  mutations: {
+    [CHANGE](state, payload) {
+      state.name = payload;
+    },
+    [INCREMENT](state, payload = 1) {
+      state.year += payload;
+    }
+  },
+  actions: {
+    [CHANGE]({ commit }, payload) {
+      commit(CHANGE, payload);
+    },
+    [INCREMENT]({ commit }, payload) {
+      commit(INCREMENT, payload);
+    }
+  }
+});
+
+// Preserve all actions (can be send to server too)
+// Allows replay of actions
+store.subscribeAction((action, state) => {
+  console.table({
+    state: JSON.parse(JSON.stringify(state)),
+    action
+  });
+});
+
+export default store;
