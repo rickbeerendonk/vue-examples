ba705d02267fe9568d1af1a93654032dd9f4cf60
Rick Beerendonk <rick@beerendonk.com>
2020-02-03 09:40:52 +0100

    Add "Vuex - with Vue - Actions - Fetch" example.

---
+++ Vuex/06 - with Vue - 5. Actions - c. Fetch/runtime-module/components/post-list-container.js
@@ -0,0 +1,34 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import {
+  mapActions,
+  mapState
+} from '../../../../node_modules/vuex/dist/vuex.esm.browser.js';
+
+import { FETCH_POSTS } from '../store.js';
+
+import ErrorMessage from './error-message.js';
+import Fetching from './fetching.js';
+import PostList from './post-list.js';
+
+export default {
+  name: 'PostListContainer',
+  components: {
+    ErrorMessage,
+    Fetching,
+    PostList
+  },
+  computed: mapState(['isFetching', 'error', 'posts']),
+  methods: mapActions([FETCH_POSTS]),
+  mounted() {
+    this[FETCH_POSTS]('resources/posts.json');
+  },
+  template: `
+    <div>
+      <Fetching v-if="isFetching" />
+      <ErrorMessage v-else-if="error" message="error" />
+      <PostList v-else :posts="posts" />
+    </div>
+  `
+};
---
+++ Vuex/06 - with Vue - 5. Actions - c. Fetch/runtime-module/main.js
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+
+import PostListContainer from './components/post-list-container.js';
+import store from './store.js';
+
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(PostListContainer)
+});
