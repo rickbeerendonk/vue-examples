0e54ef3bd9a093fc6b48e03f9a1315f4ffa86770
Rick Beerendonk <rick@beerendonk.com>
2020-02-04 08:50:38 +0100

    Consistent naming.

--- 31 - Composition API (Experimental) - TODO/1. Setup/a. Databinding/next/runtime-module/App.js
+++ 31 - Composition API (Experimental) - TODO/1. Setup/a. Databinding/next/runtime-module/App.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-import { reactive } from '../../../../node_modules/vue-next/dist/vue.esm.js';
+import { reactive } from '../../../../../node_modules/vue-next/dist/vue.esm.js';
 
 export default {
   name: 'App',
