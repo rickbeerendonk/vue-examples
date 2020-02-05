d37631601116cbc485dcb1cdad979ec4e6eb0c11
Rick Beerendonk <rick@beerendonk.com>
2020-02-05 16:56:49 +0100

    Add "Higher-Order Components - MouseMove" example.

---
+++ 23 - Higher-Order Components/b. MouseMove/current/runtime-module/hoc.js
@@ -0,0 +1,36 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+export default function(WrappedComponent) {
+  return {
+    name: 'withMouseMove',
+    data() {
+      return {
+        mousePosition: {
+          x: undefined,
+          y: undefined
+        }
+      };
+    },
+    methods: {
+      handleMouseMove(e) {
+        this.mousePosition = { x: e.pageX, y: e.pageY };
+      }
+    },
+    mounted() {
+      document.addEventListener('mousemove', this.handleMouseMove);
+    },
+    destroyed() {
+      document.removeEventListener('mousemove', this.handleMouseMove);
+    },
+    render(h) {
+      return h(WrappedComponent, {
+        props: {
+          ...this.$props,
+          name:
+            '(x:' + this.mousePosition.x + ', y: ' + this.mousePosition.y + ')'
+        }
+      });
+    }
+  };
+}
