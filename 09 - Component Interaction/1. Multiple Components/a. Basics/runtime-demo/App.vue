bcfe899d003d5fb143638d428207bb0b59d17fc1
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 18:04:48 +0100

    Add "Component Interaction - Multiple Components - Basics" example.

---
+++ 09 - Component Interaction/1. Multiple Components/a. Basics/runtime-demo/App.vue
@@ -0,0 +1,28 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <h1>App</h1>
+    <ComponentA></ComponentA>
+    <ComponentB></ComponentB>
+  </div>
+</template>
+
+<script>
+/* eslint vue/require-prop-types: "off" */
+/* global httpVueLoader */
+
+const ComponentA = httpVueLoader('./ComponentA.vue');
+const ComponentB = httpVueLoader('./ComponentB.vue');
+
+module.exports = {
+  name: 'App',
+  components: {
+    componenta: ComponentA,
+    componentb: ComponentB
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ 09 - Component Interaction/1. Multiple Components/a. Basics/runtime-demo/ComponentA.vue
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <h2>Component A</h2>
+</template>
+
+<script>
+module.exports = {
+  name: 'ComponentA'
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #d676ff;
+}
+</style>
