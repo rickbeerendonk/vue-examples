27b5259fc67726c5b3f7edf2d087aded029da750
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 14:57:12 +0100

    Add "Dynamic Components - No Match" example.

---
+++ 14 - Dynamic Components/b. No Match/current/compiled/src/App.vue
@@ -0,0 +1,54 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <label>
+      <input v-model="language" type="radio" value="dutch" />
+      Dutch
+    </label>
+    <label>
+      <input v-model="language" type="radio" value="english" />
+      English
+    </label>
+    <label>
+      <input v-model="language" type="radio" value="spanish" />
+      Spanish
+    </label>
+
+    <div id="result">
+      <component :is="greetingLanguage"></component>
+    </div>
+  </div>
+</template>
+
+<script>
+import GreetingDutch from './components/GreetingDutch.vue';
+import GreetingEnglish from './components/GreetingEnglish.vue';
+import GreetingSpanish from './components/GreetingSpanish.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'greeting-dutch': GreetingDutch,
+    'greeting-english': GreetingEnglish,
+    'greeting-spanish': GreetingSpanish
+  },
+  data() {
+    return {
+      language: null
+    };
+  },
+  computed: {
+    greetingLanguage() {
+      return this.language ? 'greeting-' + this.language : null;
+    }
+  }
+};
+</script>
+
+<style>
+#result {
+  background: bisque;
+}
+</style>
