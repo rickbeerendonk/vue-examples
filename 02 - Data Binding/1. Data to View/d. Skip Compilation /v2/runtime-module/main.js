126c508774e9d879a7232aecce4e0872820f21bc
Rick Beerendonk <rick@beerendonk.com>
2020-09-27 23:03:53 +0200

    Add "Data Binding - Data to View - Skip Compilation" example.

---
+++ 02 - Data Binding/1. Data to View/d. Skip Compilation /v2/runtime-module/main.js
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import Vue from '../../../../../node_modules/vue_2/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Hello World!'
+  }
+});
