0d662529dc9c89963688ddf34954fa2776500562
Rick Beerendonk <rick@beerendonk.com>
2020-09-27 23:45:58 +0200

    Add v3 version.

---
+++ 02 - Data Binding/3. Class/b. Array Syntax - i. Basics/v3/runtime-module/main.js
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+Vue.createApp({
+  data() {
+    return {
+      classDynamic1: 'dynamic1',
+      classDynamic2: 'dynamic2'
+    };
+  },
+  methods: {
+    clicked() {
+      this.classDynamic1 = this.classDynamic1 ? '' : 'dynamic1';
+      this.classDynamic2 = this.classDynamic2 ? '' : 'dynamic2';
+    }
+  }
+}).mount('#app');
