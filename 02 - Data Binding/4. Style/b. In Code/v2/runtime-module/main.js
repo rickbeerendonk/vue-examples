1a2406e847adc36c7ea386e8be5f6c73490ef31f
Rick Beerendonk <rick@beerendonk.com>
2020-09-28 14:11:18 +0200

    Style using camelCase vs kebab-case.

--- 02 - Data Binding/4. Style/b. In Code/v2/runtime-module/main.js
+++ 02 - Data Binding/4. Style/b. In Code/v2/runtime-module/main.js
@@ -6,10 +6,17 @@ import Vue from '../../../../../node_modules/vue_2/dist/vue.esm.browser.js';
 new Vue({
   el: '#app',
   data: {
-    style: {
+    // camelCase
+    styleCamel: {
       fontSize: '48px',
       fontStyle: 'italic',
       textDecoration: 'underline'
+    },
+    // kebab-case
+    styleKebab: {
+      ['font-size']: '48px',
+      ['font-style']: 'italic',
+      ['text-decoration']: 'underline'
     }
   }
 });
--- 02 - Data Binding/4. Style/b. In Code/v3/runtime-module/main.js
+++ 02 - Data Binding/4. Style/b. In Code/v3/runtime-module/main.js
@@ -6,10 +6,17 @@ import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js'
 Vue.createApp({
   data() {
     return {
-      style: {
+      // camelCase
+      styleCamel: {
         fontSize: '48px',
         fontStyle: 'italic',
         textDecoration: 'underline'
+      },
+      // kebab-case
+      styleKebab: {
+        ['font-size']: '48px',
+        ['font-style']: 'italic',
+        ['text-decoration']: 'underline'
       }
     };
   }
