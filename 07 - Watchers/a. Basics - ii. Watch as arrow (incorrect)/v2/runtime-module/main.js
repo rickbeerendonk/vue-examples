6f3585cc9455f81bd60970a6188b24c81aad07ad
Rick Beerendonk <rick@beerendonk.com>
2020-09-28 20:48:45 +0200

    Add "Watchers - Basics - Watch as Arrow (incorrect)" example.

---
+++ 07 - Watchers/a. Basics - ii. Watch as arrow (incorrect)/v2/runtime-module/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue_2/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    count: 0,
+    change: ''
+  },
+  watch: {
+    count: (newValue, oldValue) => {
+      // Arrow, so this is not available:
+      this.change = `Changed from ${oldValue} to ${newValue}`;
+    }
+  }
+});
---
+++ 07 - Watchers/a. Basics - ii. Watch as arrow (incorrect)/v3/runtime-modules/main.js
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+Vue.createApp({
+  data() {
+    return {
+      count: 0,
+      change: ''
+    };
+  },
+  watch: {
+    count: (newValue, oldValue) => {
+      // Arrow, so this is not available:
+      this.change = `Changed from ${oldValue} to ${newValue}`;
+    }
+  }
+}).mount('#app');
