008269748842895fff07eee9707a8d14d8ec1570
Rick Beerendonk <rick@beerendonk.com>
2020-09-28 18:20:17 +0200

    Add v3 versions. Key in v-if, v-else, v-else-if no longer needed.

---
+++ 03 - Conditional Rendering/3. Else-If/v3/runtime-module/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+Vue.createApp({
+  data() {
+    return {
+      language: 'nl'
+    };
+  }
+}).mount('#app');
