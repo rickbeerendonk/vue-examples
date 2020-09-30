f9978a9c03826a4fec78ccf8da5da95d586da2b5
Rick Beerendonk <rick@beerendonk.com>
2020-09-30 23:25:25 +0200

    Add v3 version.

---
+++ 11 - Key Modifiers/2. Key Code (deprecated)/a. Delete (and Backspace)/v3/runtime-module/main.js
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+Vue.createApp({
+  data() {
+    return {
+      result: []
+    };
+  },
+  methods: {
+    keyup: function (event) {
+      this.result.push(
+        `key: ${event.key}, code: ${event.code} (ctrlKey: ${event.ctrlKey}, shiftKey  ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+      );
+    }
+  }
+}).mount('#app');
