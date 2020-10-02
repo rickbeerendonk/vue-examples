870727e47d36a91517e6b3a63762f657c04043b4
Rick Beerendonk <rick@beerendonk.com>
2020-10-02 09:09:42 +0200

    Add "Render and JSX - Use Data" example.

---
+++ 19 - Render and JSX/e. Use Data/v2/runtime-module/main.js
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../../node_modules/vue_2/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    name: 'Vue'
+  },
+  render: function (createElement) {
+    return createElement(
+      'h1', // tag name
+      ['Hello ', this.name, '!'] // array of children
+    );
+  }
+});
