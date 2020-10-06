9cc107e65ab2fb8ea5707f91e1a463ab175141d2
Rick Beerendonk <rick@beerendonk.com>
2020-10-06 19:10:45 +0200

    Remove duplicate example.

--- 09 - Component Interaction/2. Properties/i. Transfer/v2/runtime-module/ImportantAnchor.js
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2020 Rick Beerendonk          !*/
-
-/* eslint vue/require-prop-types: "off" */
-
-export default {
-  name: 'ImportantAnchor',
-  template: '<a v-bind="$props">!!! <slot></slot> !!!</a>'
-};
