6eb013b91fc022b6c6337f94c1348a6959e2f996
Rick Beerendonk <rick@beerendonk.com>
2020-10-06 10:37:06 +0200

    Add "Conditional Rendering - If - Nested" example.

---
+++ 03 - Conditional Rendering/1. If - iii. Nested/v2/runtime-module/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue_2/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    show: true,
+    show2: true
+  }
+});
