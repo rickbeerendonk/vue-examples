e8f35c0ba0fd0a82a366ce9f68251bfbe42e64a0
Rick Beerendonk <rick@beerendonk.com>
2020-10-06 19:26:51 +0200

    Add "Component Interaction - Properties - Avoid Mutation" example.

---
+++ 09 - Component Interaction/2. Properties/i. Avoid Mutation/v2/runtime-demo/GreetingComponent.vue
@@ -0,0 +1,21 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <p style="color: gray">See console for "Avoid mutating" warning</p>
+    <input v-model="name" />
+    <h1>Hello {{ name }}!</h1>
+  </div>
+</template>
+
+<script>
+/* eslint vue/require-prop-types: "off" */
+
+module.exports = {
+  name: 'GreetingComponent',
+  props: ['name']
+};
+</script>
+
+<style scoped></style>
