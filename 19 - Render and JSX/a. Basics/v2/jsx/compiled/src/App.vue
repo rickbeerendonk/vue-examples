bd8ccd04346caf7c381c234c7ea51f9c0de44cdd
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 23:29:56 +0200

    Add JSX version.

---
+++ 19 - Render and JSX/a. Basics/v2/jsx/compiled/src/App.vue
@@ -0,0 +1,17 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script>
+export default {
+  name: 'App',
+  render() {
+    return <h1 id="app">Hello Vue!</h1>;
+  }
+};
+</script>
+
+<style>
+#app {
+  color: #444;
+}
+</style>
