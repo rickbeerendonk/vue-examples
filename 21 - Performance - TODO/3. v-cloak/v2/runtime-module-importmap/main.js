81f986b4f3a8ef47b35aee755e36964c2ddc50e1
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 21:12:48 +0200

    Add "Performance - v-cloak" example.

---
+++ 21 - Performance - TODO/3. v-cloak/v2/runtime-module-importmap/main.js
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from 'vue';
+
+new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Hello World!'
+  }
+});
