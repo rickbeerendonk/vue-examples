77f978bc2fe525a947300e2e8fd435c85dc00eaf
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 20:57:09 +0200

    Add runtime-modules-importmap versions.

---
+++ 02 - Data Binding/1. Data to View/a. Content - iii. Partial/v3/runtime-module-importmap/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+
+Vue.createApp({
+  data() {
+    return {
+      greeting: 'Hello World!'
+    };
+  }
+}).mount('#app');
