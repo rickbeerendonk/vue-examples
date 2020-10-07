6951e52429746ce0753dfef7c3d9c10d0ae659d0
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 23:51:19 +0200

    Add JSX version.

---
+++ 19 - Render and JSX/c. Attributes/v2/jsx/compiled/src/App.vue
@@ -0,0 +1,17 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script>
+export default {
+  name: 'App',
+  render() {
+    return (
+      <a href="https://vuejs.org" target="_blank">
+        Vue website
+      </a>
+    );
+  }
+};
+</script>
+
+<style></style>
