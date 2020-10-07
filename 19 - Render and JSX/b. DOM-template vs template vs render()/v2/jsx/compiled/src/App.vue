fddf61eee50470f3e5048168a261658bb6ce5f93
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 23:57:00 +0200

    Add JSX version.

---
+++ 19 - Render and JSX/b. DOM-template vs template vs render()/v2/jsx/compiled/src/App.vue
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <h1>DOM Template</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  template: '<h1>Component Template</h1>',
+  render() {
+    return <h1 id="app">Render()</h1>;
+  }
+};
+</script>
+
+<style></style>
