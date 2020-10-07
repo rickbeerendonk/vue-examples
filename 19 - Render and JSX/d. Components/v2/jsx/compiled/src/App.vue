df68978584aab9eec804df59913ae7cc01a62965
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 23:42:21 +0200

    Add JSX version.

---
+++ 19 - Render and JSX/d. Components/v2/jsx/compiled/src/App.vue
@@ -0,0 +1,15 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script>
+import Greeting from './Greeting';
+
+export default {
+  name: 'App',
+  render() {
+    return <Greeting name="World" />;
+  }
+};
+</script>
+
+<style></style>
---
+++ 19 - Render and JSX/d. Components/v2/jsx/compiled/src/Greeting.vue
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script>
+export default {
+  name: 'Greeting',
+  props: ['name'],
+  render() {
+    return <h1 id="app">Hello {this.name}!</h1>;
+  }
+};
+</script>
+
+<style>
+#app {
+  color: #444;
+}
+</style>
