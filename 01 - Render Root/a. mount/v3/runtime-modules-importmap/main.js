b62e910ec1ad4cd460a4d5fe22ce0ae27182b85c
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 20:54:16 +0200

    Add runtime-modules-importmap version.

---
+++ 01 - Render Root/a. mount/v3/runtime-modules-importmap/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+
+console.log(Vue.version);
+
+Vue.createApp({
+  template: `Hello Vue ${Vue.version}!` // No <div> needed.
+}).mount('#app1' /* // <div id="app1"></div> */);
+
+Vue.createApp({
+  data() {
+    return {
+      greeting: `Hi Vue ${Vue.version}!`
+    };
+  }
+}).mount('#app2' /* <div id="app2">{{ greeting }}</div> */);
