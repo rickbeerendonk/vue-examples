13147b3efdae53cb21b23e63aa6b9c5bf6736e7e
Rick Beerendonk <rick@beerendonk.com>
2020-10-07 23:36:42 +0200

    Add JSX version.

---
+++ 19 - Render and JSX/e. Use Data/v2/jsx/compiled/webpack.config.js
@@ -0,0 +1,59 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+/* global __dirname */
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 9100,
+    hot: true
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file),
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ from: 'public' }] }),
+    new HtmlWebpackPlugin({
+      favicon: './public/favicon.ico',
+      template: './public/index.html',
+      title: 'Render and JSX - Use Data'
+    }),
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json', '.mjs', '.vue']
+  }
+};
