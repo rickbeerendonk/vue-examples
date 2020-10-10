5c510b3838e31ae1110a61e7d5e2bef3462840d4
Rick Beerendonk <rick@beerendonk.com>
2020-10-10 22:21:14 +0200

    Add v3 version.

---
+++ Router/04 - Redirect/c. Dynamic/v3/runtime-module-importmap/main.js
@@ -0,0 +1,36 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const Home = { template: '<h2>Home</h2>' };
+const Page1 = { template: '<h2>Page 1</h2>' };
+const Page2 = {
+  template: `
+    <div>
+      <h2>Page 2</h2>
+      <ul>
+        <li>$route.name: {{ $route.name }}</li>
+        <li>$route.redirectedFrom: <pre>{{ JSON.stringify($route.redirectedFrom, null, 4) }}</pre></li>
+      </ul>
+    </div>
+  `
+};
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/page1', component: Page1 },
+    { path: '/page2', name: 'second', component: Page2 },
+    {
+      path: '/oldpage2',
+      redirect: () => (new Date().getMinutes() % 2 === 0 ? '/page2' : '/page1')
+    }
+  ]
+});
+
+const app = Vue.createApp({});
+app.use(router);
+app.mount('#app');
