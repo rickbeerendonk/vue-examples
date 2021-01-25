0cfd990c2da230b4ebea01089deb462c9a5a2258
Rick Beerendonk <rick@beerendonk.com>
2021-01-25 23:38:52 +0100

    Finish WIP examples.

---
+++ 12 - Form Input Bindings/a. Basics/4. Radio - ii. No Match/v3/runtime-module/main.js
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+const app = Vue.createApp({
+  data() {
+    return {
+      checked: 'no match'
+    };
+  }
+});
+app.mount('#app');
---
+++ 12 - Form Input Bindings/a. Basics/5. Select - iii. Dynamic/v3/runtime-module/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+const app = Vue.createApp({
+  data() {
+    return {
+      options: [
+        { description: 'Option A', value: 'A' },
+        { description: 'Option B', value: 'B' },
+        { description: 'Option C', value: 'C' }
+      ],
+      selected: 'B'
+    };
+  }
+});
+app.mount('#app');
