bd203b091602ecc2b602e23e8223d069738df188
Rick Beerendonk <rick@beerendonk.com>
2021-01-26 21:46:24 +0100

    Finish WIP examples.

--- 41 - Container Components/a. Fetch - i. sync/v3/runtime-module/main.js
+++ 41 - Container Components/a. Fetch - i. sync/v3/runtime-module/main.js
@@ -6,6 +6,8 @@ import * as Vue from '../../../../node_modules/vue_3/dist/vue.esm-browser.js';
 import PostListContainer from './components/post-list-container.js';
 
 const app = Vue.createApp({
-  render: h => h(PostListContainer)
+  render() {
+    return Vue.h(PostListContainer);
+  }
 });
 app.mount('#app');
