06ec668edf748722181a841de41fe9b0ca75c7c2
Rick Beerendonk <rick@beerendonk.com>
2021-01-28 02:12:34 +0100

    Use component directly.

--- Vuex/06 - with Vue - 2. Store to Component - a. One Value/v3/runtime-module-importmap/main.js
+++ Vuex/06 - with Vue - 2. Store to Component - a. One Value/v3/runtime-module-importmap/main.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-import { createApp, h } from 'vue';
+import { createApp } from 'vue';
 import * as Vuex from 'vuex';
 
 const store = new Vuex.Store({
@@ -21,10 +21,6 @@ const App = {
   template: '<h1>State: {{ state }}</h1>'
 };
 
-const app = createApp({
-  render() {
-    return h(App);
-  }
-});
+const app = createApp(App);
 app.use(store);
 app.mount('#app');
--- Vuex/06 - with Vue - 6. Modules - a. Namespaced/v3/runtime-module-importmap/main.js
+++ Vuex/06 - with Vue - 6. Modules - a. Namespaced/v3/runtime-module-importmap/main.js
@@ -1,15 +1,11 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-import { createApp, h } from 'vue';
+import { createApp } from 'vue';
 
 import App from './main/components/App.js';
 import store from './main/store/index.js';
 
-const app = createApp({
-  render() {
-    return h(App);
-  }
-});
+const app = createApp(App);
 app.use(store);
 app.mount('#app');
