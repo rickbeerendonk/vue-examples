c9ef8a4b214f24e159de3449b4a589dd9644150e
Rick Beerendonk <rick@beerendonk.com>
2021-02-17 21:16:51 +0100

    Fix handler.

--- 09 - Component Interaction/3. Events/a. Without Data/v3/runtime-module/Child.js
+++ 09 - Component Interaction/3. Events/a. Without Data/v3/runtime-module/Child.js
@@ -10,5 +10,5 @@ export default {
       this.$emit('updated');
     }
   },
-  template: '<button @click="buttonClicked()">Click</button>'
+  template: '<button @click="buttonClicked">Click</button>'
 };
