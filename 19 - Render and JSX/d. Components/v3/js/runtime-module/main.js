4a80099201388d87ae7bb366066943ca828a4d19
Rick Beerendonk <rick@beerendonk.com>
2021-02-18 23:45:53 +0100

    Add v3 version.

---
+++ 19 - Render and JSX/d. Components/v3/js/runtime-module/main.js
@@ -0,0 +1,21 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+const Greeting = {
+  props: ['name'],
+  template: `<h1>Hello {{ name }}!</h1>`
+};
+
+Vue.createApp({
+  render() {
+    return Vue.h(
+      Greeting, // tag name
+      {
+        name: 'World'
+      } // object with attributes
+      // [Optional] array of children
+    );
+  }
+}).mount('#app');
---
+++ 19 - Render and JSX/d. Components/v3/jsx/compiled/src/Greeting.vue
@@ -0,0 +1,22 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  render() {
+    return <h1 id="greeting">Hello {this.name}!</h1>;
+  }
+};
+</script>
+
+<style>
+#greeting {
+  color: #444;
+}
+</style>
