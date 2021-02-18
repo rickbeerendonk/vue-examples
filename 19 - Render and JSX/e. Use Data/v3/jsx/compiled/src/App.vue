fc72b33b171967a374a0e2b98b2b94669bf56dc4
Rick Beerendonk <rick@beerendonk.com>
2021-02-19 00:14:24 +0100

    Add v3 version.

---
+++ 19 - Render and JSX/e. Use Data/v3/jsx/compiled/src/App.vue
@@ -0,0 +1,21 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2021 Rick Beerendonk          -->
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue'
+    };
+  },
+  render() {
+    return <h1 id="app">Hello {this.name}!</h1>;
+  }
+};
+</script>
+
+<style>
+#app {
+  color: #444;
+}
+</style>
