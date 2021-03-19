c135a1b0654b364c242177d56c0120635e924731
Rick Beerendonk <rick@beerendonk.com>
2021-03-19 13:29:29 +0100

    Add Vue 3 version.

---
+++ Setup/v3/Webpack/ts/src/App.vue
@@ -0,0 +1,28 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div id="app">
+    <greeting name="Vue" />
+    <greeting-with-jsx name="JSX" />
+  </div>
+</template>
+
+<script>
+import Greeting from './components/Greeting.vue';
+import GreetingWithJsx from './components/GreetingWithJsx.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Greeting,
+    GreetingWithJsx
+  }
+};
+</script>
+
+<style>
+#app {
+  background-color: #444;
+}
+</style>
