e3469db0f6abce6736644cb667a5917080b3bed9
Rick Beerendonk <rick@beerendonk.com>
2021-07-24 08:09:36 +0200

    Add v3 version.

---
+++ Debugging/a. Basics/v3/webpack.config.js
@@ -0,0 +1,61 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+/* global __dirname */
+
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].[fullhash:8].js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 9100,
+    hot: true,
+    overlay: true,
+    stats: 'errors-only'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file),
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ from: 'public' }] }),
+    new HtmlWebpackPlugin({
+      favicon: './public/favicon.ico',
+      template: './src/template.ejs'
+    }),
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json', '.mjs', '.vue']
+  }
+};
