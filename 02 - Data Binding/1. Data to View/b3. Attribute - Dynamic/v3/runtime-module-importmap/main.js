6c244ad145e80e744978bbfd9ee00272aa0c2a84
Rick Beerendonk <rick@beerendonk.com>
2021-07-30 14:02:43 +0200

    Add importmap versions.

---
+++ 02 - Data Binding/1. Data to View/b3. Attribute - Dynamic/v3/runtime-module-importmap/main.js
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+
+Vue.createApp({
+  data() {
+    return {
+      dynamicattribute: 'title',
+      name: 'World'
+    };
+  }
+}).mount('#app');
