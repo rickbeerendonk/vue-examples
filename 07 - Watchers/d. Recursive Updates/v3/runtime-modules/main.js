a5afd54d5758f072dd7bf6e6abb1e4c12c599810
Rick Beerendonk <rick@beerendonk.com>
2021-08-14 20:13:07 +0200

    Add "Watchers - Recursive Updates" examples.

---
+++ 07 - Watchers/d. Recursive Updates/v3/runtime-modules/main.js
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+Vue.createApp({
+  data() {
+    return {
+      count: 0,
+      change: ''
+    };
+  },
+  watch: {
+    count(newValue, oldValue) {
+      this.change = `Changed from ${oldValue} to ${newValue}`;
+    },
+    change() {
+      /*
+      [Vue warn]: Maximum recursive updates exceeded. This means you have a reactive effect that is mutating its own dependencies and thus recursively triggering itself. Possible sources include component template, render function, updated hook or watcher source function.
+      */
+      this.count++;
+    }
+  }
+}).mount('#app');
