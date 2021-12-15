db6c373195e1bd4310d50cecaa8d2bdcbc972736
Rick Beerendonk <rick@beerendonk.com>
2021-12-16 00:33:51 +0100

    Add "Watchers - Reactive with Deep" WIP.

---
+++ 07 - Watchers/c2. Reactive with Deep [WIP oldState]/v3-composition-api/runtime-modules/main.js
@@ -0,0 +1,25 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import {
+  createApp,
+  reactive,
+  watch
+} from '../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+createApp({
+  setup() {
+    const state = reactive({ count: 0, change: '' });
+    watch(
+      () => state,
+      (state, oldState) => {
+        state.change = `Changed from ${oldState.count} to ${state.count}`;
+      },
+      {
+        deep: true
+      }
+    );
+
+    return { state };
+  }
+}).mount('#app');
