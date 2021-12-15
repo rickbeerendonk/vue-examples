5b093bac563bd251290c6821c14655aebe55232e
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 19:37:19 +0100

    Add Composition API version.

---
+++ 02 - Data Binding/1. Data to View/b2. Attribute (Shorthand)/v3-composition-api/runtime-module-importmap/main.js
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { createApp } from 'vue';
+
+createApp({
+  setup() {
+    // Return data available for rest of the component
+    return { name: 'World' };
+  }
+}).mount('#app');
