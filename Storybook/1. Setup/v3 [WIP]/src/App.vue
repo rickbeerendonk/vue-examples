7411b7d602edeaa2dac8a8582754f5ca750c9dca
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 02:36:17 +0100

    Add WIP.

---
+++ Storybook/1. Setup/v3 [WIP]/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <GreetingEditor value="Vue" />
+</template>
+
+<script>
+import GreetingEditor from './containers/GreetingEditor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GreetingEditor
+  }
+};
+</script>
+
+<style></style>
