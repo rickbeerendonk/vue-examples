f6ed3d46a9503887ed473ce967bf1983d682ede3
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 21:19:02 +0100

    Add Composition API versions.

---
+++ 03 - Conditional Rendering/7. If vs Show vs Visibility Hidden/v3-composition-api/runtime-module/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { createApp } from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+createApp({
+  setup() {
+    return {
+      show: false
+    };
+  }
+}).mount('#app');
