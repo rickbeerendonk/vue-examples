c4a1d5d23ac860186f34293e5cd2cc5dbd312732
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 19:40:45 +0100

    Add Composition API version.

---
+++ 02 - Data Binding/1. Data to View/d. Skip Compilation/v3-composition-api/runtime-module-importmap/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { createApp } from 'vue';
+
+createApp({
+  setup() {
+    return {
+      greeting: 'Hello World!'
+    };
+  }
+}).mount('#app');
