fbed3432b6772f9807acca3461e9ab71d4603cb4
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 20:20:32 +0100

    Add Composition API version.

---
+++ 02 - Data Binding/2. View to Data/c2. Events calling Methods changing Data (Shorthand)/v3-composition-api/runtime-module/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import {
+  createApp,
+  ref
+} from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+createApp({
+  setup() {
+    const count = ref(0);
+    const clicked = () => {
+      count.value++;
+    };
+
+    return { count, clicked };
+  }
+}).mount('#app');
