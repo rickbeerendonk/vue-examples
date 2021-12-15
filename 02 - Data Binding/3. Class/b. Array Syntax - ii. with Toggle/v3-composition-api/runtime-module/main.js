98c90ed1db6e8e41ac42588ac154b366ea661f00
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 21:06:05 +0100

    Add Composition API version.

---
+++ 02 - Data Binding/3. Class/b. Array Syntax - ii. with Toggle/v3-composition-api/runtime-module/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import {
+  createApp,
+  ref
+} from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+createApp({
+  setup() {
+    const isDynamic = ref(false);
+    const clicked = () => {
+      isDynamic.value = !isDynamic.value;
+    };
+
+    return { isDynamic, clicked };
+  }
+}).mount('#app');
