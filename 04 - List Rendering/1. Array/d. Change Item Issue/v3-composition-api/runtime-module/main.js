490e161a225d31132b13e9d80e6ed4d19ce5736f
Rick Beerendonk <rick@beerendonk.com>
2021-12-15 21:29:17 +0100

    Add Composition API versions.

---
+++ 04 - List Rendering/1. Array/d. Change Item Issue/v3-composition-api/runtime-module/main.js
@@ -0,0 +1,37 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import {
+  createApp,
+  ref
+} from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+let itemId = 4;
+
+createApp({
+  setup() {
+    const items = ref(['one', 'two', 'three']);
+
+    const overwrite1 = () => {
+      // WRONG in Vue 3 Composition API
+      //       because there is no set function
+      // set(this.items, 0, itemId++);
+    };
+    const overwrite2 = () => {
+      items.value.splice(0, 1, itemId++);
+    };
+    const overwrite3 = () => {
+      const [, ...rest] = items.value;
+      items.value = [itemId++, ...rest];
+    };
+    const overwrite4 = () => {
+      // RIGHT in Vue 3, was wrong in Vue 2,
+      // because it uses Proxies (this here is a Proxy):
+      //console.log(this);
+
+      items.value[0] = itemId++;
+    };
+
+    return { items, overwrite1, overwrite2, overwrite3, overwrite4 };
+  }
+}).mount('#app');
---
+++ 04 - List Rendering/1. Array/e. Index as Key Issue/v3-composition-api/runtime-module/main.js
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import {
+  createApp,
+  ref
+} from '../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+let itemId = 4;
+
+createApp({
+  setup() {
+    const items = ref(['one', 'two', 'three']);
+    const insertTop = () => {
+      items.value.splice(0, 0, itemId++);
+    };
+
+    return { items, insertTop };
+  }
+}).mount('#app');
