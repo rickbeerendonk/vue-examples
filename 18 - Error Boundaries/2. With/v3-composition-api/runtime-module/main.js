86738a4ccdf32f5fde164ff063bbdde4d07c753c
Rick Beerendonk <rick@beerendonk.com>
2021-12-16 02:57:52 +0100

    Add Composition API version.

---
+++ 18 - Error Boundaries/2. With/v3-composition-api/runtime-module/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import * as Vue from '../../../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+import DividerComponent from './DividerComponent.js';
+import ErrorBoundary from './ErrorBoundary.js';
+
+const app = Vue.createApp({
+  components: { DividerComponent, ErrorBoundary },
+  template: `
+    <h1>Error Boundaries</h1>
+    <ErrorBoundary>
+      <DividerComponent />
+    </ErrorBoundary>
+  `
+});
+app.mount('#app');
