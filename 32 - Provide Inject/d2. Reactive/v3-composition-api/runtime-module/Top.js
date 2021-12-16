b886cb0bda62462cf1586fd607694002984c6967
Rick Beerendonk <rick@beerendonk.com>
2021-12-16 14:15:25 +0100

    Add Composition API version.

---
+++ 32 - Provide Inject/d2. Reactive/v3-composition-api/runtime-module/Top.js
@@ -0,0 +1,33 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import {
+  ref,
+  provide
+} from '../../../../node_modules/vue_3/dist/vue.esm-browser.js';
+
+import Middle from './Middle.js';
+
+export default {
+  name: 'Top',
+  props: {
+    initialColor: String
+  },
+  setup(props) {
+    const color = ref(props.initialColor);
+    const switchColor = () => {
+      color.value = color.value === 'red' ? 'green' : 'red';
+    };
+
+    provide('color', color);
+
+    return { color, switchColor };
+  },
+  components: {
+    Middle
+  },
+  template: `
+    <Middle />
+    <button @click="switchColor">Switch (current: {{color}})</button>
+  `
+};
