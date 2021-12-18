1a22eeb8628a30064e7fe254062c12e6e9936f8d
Rick Beerendonk <rick@beerendonk.com>
2021-12-18 16:03:02 +0100

    Add "Dynamic Components - Basics - Using Async Components" example.

---
+++ 14 - Dynamic Components/a. Basics - ii. Using Async Components/v3/runtime-module/GreetingDutch.js
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+export default {
+  name: 'GreetingDutch',
+  data() {
+    return {
+      greeting: 'Wereld'
+    };
+  },
+  template: `
+    <label>Dutch: <input v-model="greeting" /></label>
+    <p>Hallo {{ greeting }}</p>
+  `
+};
