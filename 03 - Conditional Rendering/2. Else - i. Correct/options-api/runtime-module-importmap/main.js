48709230fc3cf3b8e344a29ccde6f30ae1dbbc43
Rick Beerendonk <rick@beerendonk.com>
2023-02-09 02:29:39 +0100

    Add importmap version.

---
+++ 03 - Conditional Rendering/2. Else - i. Correct/options-api/runtime-module-importmap/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+
+Vue.createApp({
+  data() {
+    return {
+      show: false
+    };
+  }
+}).mount('#app');
