1690712df8d22568c55b25947f70ae5910a3c036
Rick Beerendonk <rick@beerendonk.com>
2023-02-09 02:26:39 +0100

    Add importmap version.

---
+++ 04 - List Rendering/1. Array/a. For/options-api/runtime-module-importmap/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+
+Vue.createApp({
+  data() {
+    return {
+      items: ['one', 'two', 'three']
+    };
+  }
+}).mount('#app');
