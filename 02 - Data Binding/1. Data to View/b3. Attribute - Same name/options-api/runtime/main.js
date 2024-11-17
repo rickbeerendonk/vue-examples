93669fc4dcd4d0ea0f8081f628dda96701a6171d
Rick Beerendonk <rick@beerendonk.com>
2024-11-17 14:23:58 +0100

    Add "DataBinding - Data to View - Attribute - Same name" example.

---
+++ 02 - Data Binding/1. Data to View/b3. Attribute - Same name/options-api/runtime/main.js
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+import * as Vue from 'vue';
+
+Vue.createApp({
+  data() {
+    return {
+      title: 'World'
+    };
+  }
+}).mount('#app');
