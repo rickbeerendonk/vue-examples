85a1ffb9dd3759f4d4abb3c5a6e02009f1d1e07d
Rick Beerendonk <rick@beerendonk.com>
2024-11-19 12:59:35 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/2. Properties/b. Types/ii. All/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,25 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import MyComponent from './components/MyComponent.vue';
+</script>
+
+<template>
+  <h1>App</h1>
+  <MyComponent
+    str="World"
+    :num="123"
+    :bool="true"
+    :arr="[1, true, 'one']"
+    :obj="{ a: 1, b: 2 }"
+    :date="new Date()"
+    :fun="
+      function (a) {
+        return a + a;
+      }
+    "
+  />
+</template>
+
+<style scoped></style>
---
+++ 08 - Component Interaction/2. Properties/b. Types/ii. All/composition-api/compiled/js/src/components/MyComponent.vue
@@ -0,0 +1,32 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  str: String,
+  num: Number,
+  bool: Boolean,
+  arr: Array,
+  obj: Object,
+  date: Date,
+  fun: Function
+  //sym: Symbol
+});
+</script>
+
+<template>
+  <h1>Hello</h1>
+  <ul>
+    <li>String: {{ str }}</li>
+    <li>Number: {{ num }}</li>
+    <li>Boolean: {{ bool }}</li>
+    <li>Array: {{ JSON.stringify(arr) }}</li>
+    <li>Object: {{ JSON.stringify(obj) }}</li>
+    <li>Date: {{ date }}</li>
+    <li>Function: {{ fun.toString() }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
