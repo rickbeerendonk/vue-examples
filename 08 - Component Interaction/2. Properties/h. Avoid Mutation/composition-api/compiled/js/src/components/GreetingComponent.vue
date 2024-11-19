ebffa5448e1e4078bda3237f89180764dea7dbd8
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 00:54:24 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/2. Properties/h. Avoid Mutation/composition-api/compiled/js/src/components/GreetingComponent.vue
@@ -0,0 +1,21 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<!-- eslint-disable vue/no-mutating-props -->
+
+<template>
+  <p style="color: gray">See console for "Avoid mutating" warning</p>
+  <input v-model="name" />
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<style scoped></style>
