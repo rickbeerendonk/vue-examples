104ae6af9c16af425201aafb61a6d75378c2d065
Rick Beerendonk <rick@beerendonk.com>
2024-11-19 12:53:01 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/2. Properties/a. Basics/composition-api/compiled/js/src/components/GreetingComponent.vue
@@ -0,0 +1,16 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  name
+});
+</script>
+
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<style scoped></style>
