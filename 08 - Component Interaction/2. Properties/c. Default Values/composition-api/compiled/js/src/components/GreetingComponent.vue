33c77c429ca6c8de63dba44e846ed6383c47ad5c
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 00:25:31 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/2. Properties/c. Default Values/composition-api/compiled/js/src/components/GreetingComponent.vue
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+// defineProps is a compiler micro (no need to import)
+defineProps({
+  name: {
+    type: String,
+    default: 'World'
+  }
+});
+</script>
+
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<style scoped></style>
