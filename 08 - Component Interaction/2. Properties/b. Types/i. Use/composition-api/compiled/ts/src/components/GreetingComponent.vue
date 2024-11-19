2cee8de8317d107f3f9ef0c461e62e1eaf702e84
Rick Beerendonk <rick@beerendonk.com>
2024-11-19 23:43:17 +0100

    Fix examples.

--- 08 - Component Interaction/2. Properties/b. Types/i. Use/composition-api/compiled/ts/src/components/GreetingComponent.vue
+++ 08 - Component Interaction/2. Properties/b. Types/i. Use/composition-api/compiled/ts/src/components/GreetingComponent.vue
@@ -1,6 +1,8 @@
 <!-- European Union Public License version 1.2 -->
 <!-- Copyright © 2020 Rick Beerendonk          -->
 
+<!-- eslint-disable vue/valid-define-props -->
+
 <script setup lang="ts">
 interface Props {
   name: string;
