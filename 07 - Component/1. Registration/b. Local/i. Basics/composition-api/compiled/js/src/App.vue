999b45df421b34a65dd28d1a4cbbfd4f9f24dfa4
Rick Beerendonk <rick@beerendonk.com>
2024-11-19 09:40:46 +0100

    Add composition-api version.

---
+++ 07 - Component/1. Registration/b. Local/i. Basics/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,12 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import GreetingComponent from './components/GreetingComponent.vue';
+</script>
+
+<template>
+  <GreetingComponent />
+</template>
+
+<style scoped></style>
