80e9550bf27e4b2cf181f56a8a8dc1a7e4d7e4a3
Rick Beerendonk <rick@beerendonk.com>
2024-11-19 10:14:44 +0100

    Add composition-api version.

---
+++ 07 - Component/3. Template Single Root/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,13 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <!-- Surrounding div not required due to Fragments! -->
+  <h1>Hello World!</h1>
+  What do you think about Vue?
+  <div>It looks amazing.</div>
+</template>
+
+<style scoped></style>
