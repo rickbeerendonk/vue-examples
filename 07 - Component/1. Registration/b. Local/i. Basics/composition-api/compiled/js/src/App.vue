8fff647f2acd0f6bc7e934110d5637dfcd574781
Rick Beerendonk <rick@beerendonk.com>
2024-11-19 10:04:58 +0100

    Complete component.

--- 07 - Component/1. Registration/b. Local/i. Basics/composition-api/compiled/js/src/App.vue
+++ 07 - Component/1. Registration/b. Local/i. Basics/composition-api/compiled/js/src/App.vue
@@ -2,11 +2,19 @@
 <!-- Copyright © 2018 Rick Beerendonk          -->
 
 <script setup>
+import { ref } from 'vue';
 import GreetingComponent from './components/GreetingComponent.vue';
+
+const appName = ref('App');
 </script>
 
 <template>
+  <h1>{{ appName }}</h1>
   <GreetingComponent />
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
