e678a766ab0120c3e2a0209cda371b5e2d0907fe
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 10:43:34 +0100

    Add composition-api version.

---
+++ 11 - Provide Inject/c1. No Providers - Default Value/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,13 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import TopComponent from './components/TopComponent.vue';
+</script>
+
+<template>
+  <h1>Top</h1>
+  <TopComponent />
+</template>
+
+<style scoped></style>
---
+++ 11 - Provide Inject/c1. No Providers - Default Value/composition-api/compiled/js/src/components/TopComponent.vue
@@ -0,0 +1,20 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script setup>
+//import { provide } from 'vue';
+
+import MiddleComponent from './MiddleComponent.vue';
+
+/*
+  // Uncomment to see the default isn't used
+  provide('color', 'red');  
+*/
+</script>
+
+<template>
+  <h1>Top</h1>
+  <MiddleComponent />
+</template>
+
+<style scoped></style>
