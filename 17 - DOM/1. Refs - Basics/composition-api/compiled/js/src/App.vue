eda2b3f2dda5b50caf7a15f670d55e4cc9d76e52
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 13:47:14 +0100

    Add composition-api version.

---
+++ 17 - DOM/1. Refs - Basics/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,22 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref(null);
+
+function changed(e) {
+  // Use result to get direct DOM access
+  result.value.innerText = `Hello ${e.target.value}!`;
+}
+</script>
+
+<template>
+  <input @keyup="changed" />
+
+  <!-- Connect result with the h1 -->
+  <h1 ref="result"></h1>
+</template>
+
+<style scoped></style>
