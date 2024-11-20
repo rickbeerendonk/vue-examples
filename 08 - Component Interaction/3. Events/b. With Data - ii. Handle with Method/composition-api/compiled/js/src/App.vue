0f94f8fcd26c064a2225cefdadca2f065bfd3dd9
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 01:46:29 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/3. Events/b. With Data - ii. Handle with Method/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,19 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2021 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+
+const greetingText = ref('World');
+function greetingChanged(value) {
+  greetingText.value = value;
+}
+</script>
+
+<template>
+  <ChildComponent :name="greetingText" @change="greetingChanged" />
+  <h1>Hello {{ greetingText }}</h1>
+</template>
+
+<style scoped></style>
