9ab56f3180b89db98e43340f292ddb719b0d9c0c
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 23:21:18 +0100

    Add composition-api version.

---
+++ Router/02 - Named Routes/composition-api/compiled/js/src/router/index.js
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeView from '../views/HomeView.vue';
+import Page1View from '../views/Page1View.vue';
+import Page2View from '../views/Page2View.vue';
+
+const router = createRouter({
+  // Use HTML5 history mode
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'index', component: HomeView },
+    { path: '/page1', name: 'first', component: Page1View },
+    { path: '/page2', name: 'second', component: Page2View }
+  ]
+});
+
+export default router;
