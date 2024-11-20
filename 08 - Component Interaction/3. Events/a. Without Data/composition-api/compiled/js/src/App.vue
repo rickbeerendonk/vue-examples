053097a129bdff23eb4f8a0626517dbc8ed7021c
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 01:20:30 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/3. Events/a. Without Data/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,19 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2021 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+
+const count = ref(0);
+const childChanged = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <ChildComponent @updated="childChanged" />
+  <h1>Clicked: {{ count }}</h1>
+</template>
+
+<style scoped></style>
---
+++ 08 - Component Interaction/3. Events/a. Without Data/composition-api/compiled/js/src/components/ChildComponent.vue
@@ -0,0 +1,16 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+const emit = defineEmits(['updated']);
+
+function buttonClicked() {
+  emit('updated');
+}
+</script>
+
+<template>
+  <button @click="buttonClicked">Click</button>
+</template>
+
+<style scoped></style>
