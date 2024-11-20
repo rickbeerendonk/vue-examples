b18a064bd8515b5a2dd918abbdde7eebc54691c7
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 10:39:19 +0100

    Add composition-api version.

---
+++ 11 - Provide Inject/b. With/composition-api/compiled/js/src/components/MiddleComponent.vue
@@ -0,0 +1,15 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script setup>
+import Bottom1Component from './Bottom1Component.vue';
+import Bottom2Component from './Bottom2Component.vue';
+</script>
+
+<template>
+  <h2>Middle</h2>
+  <Bottom1Component />
+  <Bottom2Component />
+</template>
+
+<style scoped></style>
---
+++ 11 - Provide Inject/b. With/composition-api/compiled/js/src/components/TopComponent.vue
@@ -0,0 +1,24 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script setup>
+import { provide } from 'vue';
+
+import MiddleComponent from './MiddleComponent.vue';
+
+const props = defineProps({
+  color: {
+    type: String,
+    required: true
+  }
+});
+
+provide('color', props.color);
+</script>
+
+<template>
+  <h1>Top</h1>
+  <MiddleComponent />
+</template>
+
+<style scoped></style>
