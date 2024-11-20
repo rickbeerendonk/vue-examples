b01da500ced302c5ff6b65c2a647ed656b7be43b
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 23:52:48 +0100

    Add composition-api versions.

---
+++ Router/04 - Redirect/a. URL/composition-api/compiled/js/src/views/Page2View.vue
@@ -0,0 +1,14 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <h2>Page 2</h2>
+  <div>
+    $route.redirectedFrom:
+    <pre>{{ JSON.stringify($route.redirectedFrom, null, 4) }}</pre>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Router/04 - Redirect/c. Dynamic/composition-api/compiled/js/src/views/Page2View.vue
@@ -0,0 +1,17 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <h2>Page 2</h2>
+  <ul>
+    <li>$route.name: {{ $route.name }}</li>
+    <li>
+      $route.redirectedFrom:
+      <pre>{{ JSON.stringify($route.redirectedFrom, null, 4) }}</pre>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
