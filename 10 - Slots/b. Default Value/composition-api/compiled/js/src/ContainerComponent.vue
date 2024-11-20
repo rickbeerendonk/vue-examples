7f8a0134d22432c954870608d1a2c607c2c2af74
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 04:50:21 +0100

    Add composition-api version.

---
+++ 10 - Slots/b. Default Value/composition-api/compiled/js/src/ContainerComponent.vue
@@ -0,0 +1,25 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <div class="outer">
+    The only item:
+    <div class="inner">
+      <slot>Default Slot Value</slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer {
+  background-color: lightgray;
+  padding: 10px;
+}
+
+.inner {
+  background-color: white;
+  padding: 10px;
+}
+</style>
