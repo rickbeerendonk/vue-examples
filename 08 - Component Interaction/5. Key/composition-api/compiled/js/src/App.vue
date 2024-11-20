db72312b86f62a84d8f2135ce7ad3d8dccdb432f
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 02:06:34 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/5. Key/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,17 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2021 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+import ClickMe from './components/ClickMe.vue';
+
+const id = ref(0);
+</script>
+
+<template>
+  <button @click="id++">Change ID</button>
+  <br />
+  <ClickMe :key="id" />
+</template>
+
+<style scoped></style>
---
+++ 08 - Component Interaction/5. Key/composition-api/compiled/js/src/components/ClickMe.vue
@@ -0,0 +1,16 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <button @click.prevent="count++">
+    This link has been clicked {{ count }} times.
+  </button>
+</template>
+
+<style scoped></style>
