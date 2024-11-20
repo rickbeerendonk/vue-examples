0ec78a00ff5b4afd701a5b807a0644f84cc7d41e
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 05:21:45 +0100

    Add composition-api version.

---
+++ 10 - Slots/e. Dynamic Names/composition-api/compiled/js/src/ContainerComponent.vue
@@ -0,0 +1,29 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <div class="outer">
+    The 1st item:
+    <div class="inner">
+      <slot name="first" />
+    </div>
+    The 2nd item:
+    <div class="inner">
+      <slot name="second" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer {
+  background-color: lightgray;
+  padding: 10px;
+}
+
+.inner {
+  background-color: white;
+  padding: 10px;
+}
+</style>
