9b64ed487fc2543a45e21cd9fe8da32c647a306a
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 16:24:33 +0100

    Add composite-api version.

---
+++ 12 - Event Modifiers/2. Stop/a. Without/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,31 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref([]);
+
+function parentClicked(event) {
+  result.value.push(
+    `${event.currentTarget.id} received event for ${event.target.id}.`
+  );
+}
+
+function childClicked(event) {
+  result.value.push(`${event.target.id} received event.`);
+}
+</script>
+
+<template>
+  <div id="parent" @click="parentClicked">
+    Parent
+    <div id="child" @click="childClicked">Child</div>
+  </div>
+
+  <ul style="background-color: lightgray">
+    <li v-for="item in result" :key="item">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ 12 - Event Modifiers/2. Stop/b. With/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,38 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref([]);
+
+function parentClicked(event) {
+  result.value.push(
+    `${event.currentTarget.id} received event for ${event.target.id}.`
+  );
+}
+function childClicked(event) {
+  result.value.push(
+    `${event.target.id} received event (Standard) -- with stop.`
+  );
+}
+function childSelfClicked(event) {
+  result.value.push(`${event.target.id} received event (Self).`);
+}
+</script>
+
+<template>
+  <div id="parent" @click="parentClicked">
+    Parent
+    <!-- Order of @click.self and @click matters. -->
+    <div id="child" @click.stop="childClicked" @click.self="childSelfClicked">
+      Child
+    </div>
+  </div>
+
+  <ul style="background-color: lightgray">
+    <li v-for="item in result" :key="item">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
