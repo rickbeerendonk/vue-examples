479493ab13ea286b11c28a1f5e8a74cde0eeb23e
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 01:36:53 +0100

    Add composition-api version.

---
+++ 08 - Component Interaction/3. Events/b. With Data - i. Handle inline/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,16 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2021 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+
+const greetingText = ref('World');
+</script>
+
+<template>
+  <ChildComponent :name="greetingText" @change="greetingText = $event" />
+  <h1>Hello {{ greetingText }}</h1>
+</template>
+
+<style scoped></style>
---
+++ 08 - Component Interaction/3. Events/b. With Data - i. Handle inline/composition-api/compiled/js/src/components/ChildComponent.vue
@@ -0,0 +1,22 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+const emit = defineEmits(['change']);
+
+function changed(e) {
+  emit('change', e.target.value);
+}
+</script>
+
+<template>
+  <input :value="name" @keyup="changed" />
+</template>
+
+<style scoped></style>
