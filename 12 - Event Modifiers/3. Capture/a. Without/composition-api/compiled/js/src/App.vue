6cba9e07416c4877f9144a6e2020389c789d845e
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 16:29:03 +0100

    Add composition-api version.

---
+++ 12 - Event Modifiers/3. Capture/a. Without/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,37 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref([]);
+
+function parentAtTargetClicked(event) {
+  result.value.push(`${event.target.id} received event in at target fase.`);
+}
+function parentBubblingClicked(event) {
+  result.value.push(
+    `${event.currentTarget.id} received event for ${event.target.id} in bubbling fase.`
+  );
+}
+function childClicked(event) {
+  result.value.push(`${event.target.id} received event.`);
+}
+</script>
+
+<template>
+  <div
+    id="parent"
+    @click.self="parentAtTargetClicked"
+    @click="parentBubblingClicked"
+  >
+    Parent
+    <div id="child" @click="childClicked">Child</div>
+  </div>
+
+  <ul style="background-color: lightgray">
+    <li v-for="item in result" :key="item">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
