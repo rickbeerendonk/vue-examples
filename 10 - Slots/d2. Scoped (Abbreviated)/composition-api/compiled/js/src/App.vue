a0b35a03e0ab123e457d748734957dbcf2489cb6
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 05:14:26 +0100

    Add composition-api version.

---
+++ 10 - Slots/d2. Scoped (Abbreviated)/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,14 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import ContainerComponent from './ContainerComponent.vue';
+</script>
+
+<template>
+  <ContainerComponent>
+    <template #default="slotProps">{{ slotProps.source }}</template>
+  </ContainerComponent>
+</template>
+
+<style scoped></style>
---
+++ 10 - Slots/d2. Scoped (Abbreviated)/composition-api/compiled/js/src/ContainerComponent.vue
@@ -0,0 +1,29 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const source = ref('Container');
+</script>
+
+<template>
+  <div class="outer">
+    Scoped:
+    <div class="inner">
+      <slot :source="source" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer {
+  background-color: lightgray;
+  padding: 10px;
+}
+
+.inner {
+  background-color: white;
+  padding: 10px;
+}
+</style>
