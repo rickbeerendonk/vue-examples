2ef3b1df574dc170f52a51da0d7e8d1f44cff928
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 05:10:52 +0100

    Add composition-api version.

---
+++ 10 - Slots/d1. Scoped - ii. Multiple Values/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,20 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+import ContainerComponent from './ContainerComponent.vue';
+
+const source = ref('Root');
+</script>
+
+<template>
+  <ContainerComponent>
+    <template #first>{{ source }}</template>
+    <template #second="slotProps">
+      {{ slotProps.source }} - {{ slotProps.other }}
+    </template>
+  </ContainerComponent>
+</template>
+
+<style scoped></style>
---
+++ 10 - Slots/d1. Scoped - ii. Multiple Values/composition-api/compiled/js/src/ContainerComponent.vue
@@ -0,0 +1,34 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const source = ref('Container');
+const other = ref('Other');
+</script>
+
+<template>
+  <div class="outer">
+    Normal:
+    <div class="inner">
+      <slot name="first" />
+    </div>
+    Scoped:
+    <div class="inner">
+      <slot name="second" :source="source" :other="other" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer {
+  background-color: lightgray;
+  padding: 10px;
+}
+
+.inner {
+  background-color: white;
+  padding: 10px;
+}
+</style>
