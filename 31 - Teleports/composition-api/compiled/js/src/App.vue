8b31ba40d3d7923c7bb9d2f88b167d706ce82f8a
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 12:58:37 +0100

    Add composition-api version.

---
+++ 31 - Teleports/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,11 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  This is the Vue app.
+  <teleport to="#portal"> I am shown outside the Vue root element. </teleport>
+</template>
+
+<style scoped></style>
