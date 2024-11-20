51308ac7fcf66b72690430c214fbb960e291e115
Rick Beerendonk <rick@beerendonk.com>
2024-11-21 00:04:41 +0100

    Add composition-api version.

---
+++ Router/05 - Dynamic Routing/b. Multiple Segments/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <h1>Router</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/page/1/a">Page 1a</RouterLink>
+      <RouterLink to="/page/1/b">Page 1b</RouterLink>
+      <RouterLink to="/page/2/a">Page 2a</RouterLink>
+      <RouterLink to="/page/2/b">Page 2b</RouterLink>
+      <RouterLink to="/page/2/c">Page 2c</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView></RouterView>
+  </main>
+
+  <footer>My copyright</footer>
+</template>
+
+<style scoped>
+header,
+footer {
+  background: lightblue;
+  padding: 0.5rem;
+}
+
+main {
+  padding: 0.5rem;
+}
+
+header h1,
+header nav {
+  margin-block-start: 0;
+  margin-block-end: 0;
+}
+
+header nav > a {
+  margin-right: 0.5rem;
+}
+</style>
---
+++ Router/05 - Dynamic Routing/b. Multiple Segments/composition-api/compiled/js/src/views/PageView.vue
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <h2>Page {{ $route.params.id }}{{ $route.params.sub }}</h2>
+  <ul>
+    <li>$route.fullPath: {{ $route.fullPath }}</li>
+    <li>$route.path: {{ $route.path }}</li>
+    <li>$route.params: {{ JSON.stringify($route.params) }}</li>
+    <li>$route.query: {{ JSON.stringify($route.query) }}</li>
+    <li>$route.name: {{ $route.name }}</li>
+    <li>$route.redirectedFrom: {{ $route.redirectedFrom }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
