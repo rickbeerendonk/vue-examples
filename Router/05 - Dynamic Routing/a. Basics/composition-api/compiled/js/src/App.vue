3f735908a54488f64a1fb3a51a8b444d8ae03115
Rick Beerendonk <rick@beerendonk.com>
2024-11-20 23:58:49 +0100

    Add composition-api version.

---
+++ Router/05 - Dynamic Routing/a. Basics/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <h1>Router</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/page/1">Page 1</RouterLink>
+      <RouterLink to="/page/2">Page 2</RouterLink>
+      <RouterLink to="/page/3">Page 3</RouterLink>
+      <RouterLink to="/page/4">Page 4</RouterLink>
+      <RouterLink to="/page/5">Page 5</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView></RouterView>
+  </main>
+
+  <footer>My copyright</footer>
+</template>
+
+<style scoped>
+header,
+footer {
+  background: lightblue;
+  padding: 0.5rem;
+}
+
+main {
+  padding: 0.5rem;
+}
+
+header h1,
+header nav {
+  margin-block-start: 0;
+  margin-block-end: 0;
+}
+
+header nav > a {
+  margin-right: 0.5rem;
+}
+</style>
---
+++ Router/05 - Dynamic Routing/a. Basics/composition-api/compiled/js/src/views/PageView.vue
@@ -0,0 +1,14 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup></script>
+
+<template>
+  <h2>Page {{ $route.params.id }}</h2>
+  <div>
+    $route.params:
+    <pre>{{ JSON.stringify($route.params, null, 4) }}</pre>
+  </div>
+</template>
+
+<style scoped></style>
