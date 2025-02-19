933ac48e25c3503ca98419ff27f9c286253d481e
Rick Beerendonk <rick@beerendonk.com>
2025-02-19 22:35:56 +0100

    Remove v2 comments.

--- Router/01 - Simple/a. Hash/options-api/runtime/main.js
+++ Router/01 - Simple/a. Hash/options-api/runtime/main.js
@@ -1,10 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-// To use Vue Router v4 in the browser:
-// Shim: https://github.com/guybedford/es-module-shims
-// Or Chrome: chrome://flags/#enable-experimental-productivity-features
-
 import * as Vue from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 
@@ -12,9 +8,7 @@ const Home = { template: '<h2>Home</h2>' };
 const Page1 = { template: '<h2>Page 1</h2>' };
 const Page2 = { template: '<h2>Page 2</h2>' };
 
-// [v2] new VueRouter()
 const router = createRouter({
-  // [v2] mode: 'hash'
   history: createWebHashHistory(),
 
   routes: [
@@ -25,8 +19,6 @@ const router = createRouter({
 });
 
 const app = Vue.createApp({});
-
-// [v2] Vue.use(VueRouter)
 app.use(router);
 
 app.mount('#app');
