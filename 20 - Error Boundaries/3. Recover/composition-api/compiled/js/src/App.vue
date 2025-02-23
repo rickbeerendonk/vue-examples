75685518d0f826356099380bb6890d89a012c485
Rick Beerendonk <rick@beerendonk.com>
2025-02-23 12:30:41 +0100

    Add composition api version.

---
+++ 20 - Error Boundaries/3. Recover/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,16 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import DividerComponent from './components/DividerComponent.vue';
+import ErrorBoundary from './components/ErrorBoundary.vue';
+</script>
+
+<template>
+  <h1>App</h1>
+  <ErrorBoundary>
+    <DividerComponent />
+  </ErrorBoundary>
+</template>
+
+<style scoped></style>
---
+++ 20 - Error Boundaries/3. Recover/composition-api/compiled/js/src/components/DividerComponent.vue
@@ -0,0 +1,29 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <p style="color: gray">Divide by zero to throw an exception</p>
+    <input v-model.number="numerator" type="number" />
+    <span> divided by </span>
+    <input v-model.number="denominator" autofocus type="number" />
+    <span> is equal to </span>
+    {{ result }}.
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const numerator = ref(3);
+const denominator = ref(2);
+
+const result = computed(() => {
+  if (denominator.value === 0) {
+    throw new Error('Division by zero.');
+  }
+  return numerator.value / denominator.value;
+});
+</script>
+
+<style scoped></style>
