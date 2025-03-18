8427109c1e437e7f1445d7c249069ea0dbbb8eaf
Rick Beerendonk <rick@beerendonk.com>
2025-03-18 02:34:15 +0100

    Add composition api versions.

---
+++ 13 - Key Modifiers/2. Key Code/b. Space/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,24 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref([]);
+
+const keyup = event => {
+  result.value.push(
+    `key: ${event.key}, code: ${event.code} (ctrlKey: ${event.ctrlKey}, shiftKey: ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+  );
+};
+</script>
+
+<template>
+  <input :autofocus="'autofocus'" @keyup.space="keyup" />
+
+  <ul style="background-color: lightgray">
+    <li v-for="(item, index) in result" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
