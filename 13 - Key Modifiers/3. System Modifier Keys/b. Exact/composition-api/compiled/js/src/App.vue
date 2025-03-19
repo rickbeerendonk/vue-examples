e4ca90d7a0008b9ba318a8a9a0a1fd5df6a88e8f
Rick Beerendonk <rick@beerendonk.com>
2025-03-19 02:46:34 +0100

    Add composition api versions.

---
+++ 13 - Key Modifiers/3. System Modifier Keys/b. Exact/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,31 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref([]);
+
+const keyup = event => {
+  result.value.push(
+    `key: ${event.key}, code: ${event.code} (ctrlKey: ${event.ctrlKey}, shiftKey: ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+  );
+};
+
+const mousedown = event => {
+  result.value.push(
+    `button: ${event.button}, (ctrlKey: ${event.ctrlKey}, shiftKey  ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+  );
+};
+</script>
+
+<template>
+  <input :autofocus="'autofocus'" @keyup.ctrl.exact="keyup" />
+  <button @mousedown.ctrl.exact="mousedown">Mouse-click</button>
+
+  <ul style="background-color: lightgray">
+    <li v-for="(item, index) in result" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ 13 - Key Modifiers/3. System Modifier Keys/c1. Mouse Left/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,26 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
+
+<script setup>
+import { ref } from 'vue';
+
+const result = ref([]);
+
+const mousedown = event => {
+  result.value.push(
+    `button: ${event.button}, (ctrlKey: ${event.ctrlKey}, shiftKey  ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+  );
+};
+</script>
+
+<template>
+  <h1>Use Mouse Left</h1>
+
+  <button @mousedown.left="mousedown">Mouse-click</button>
+
+  <ul style="background-color: lightgray">
+    <li v-for="(item, index) in result" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
