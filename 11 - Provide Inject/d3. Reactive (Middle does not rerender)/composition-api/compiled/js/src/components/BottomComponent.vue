8336af3609a2e58f87885464d29bcb17f9ce534d
Rick Beerendonk <rick@beerendonk.com>
2025-03-26 12:12:01 +0100

    Add "Provide Inject - Reactive (Middle does not rerender)" example.

---
+++ 11 - Provide Inject/d3. Reactive (Middle does not rerender)/composition-api/compiled/js/src/components/BottomComponent.vue
@@ -0,0 +1,14 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script setup>
+import { inject } from 'vue';
+
+const myColor = inject('color');
+</script>
+
+<template>
+  <h3 :style="{ color: myColor }">Bottom {{ Date.now() }}</h3>
+</template>
+
+<style scoped></style>
---
+++ 11 - Provide Inject/d3. Reactive (Middle does not rerender)/composition-api/compiled/js/src/components/TopComponent.vue
@@ -0,0 +1,30 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script setup>
+import { provide, ref } from 'vue';
+
+import MiddleComponent from './MiddleComponent.vue';
+
+const props = defineProps({
+  initialColor: {
+    type: String,
+    required: true
+  }
+});
+
+const color = ref(props.initialColor);
+function switchColor() {
+  color.value = color.value === 'red' ? 'blue' : 'red';
+}
+
+provide('color', color);
+</script>
+
+<template>
+  <h1>Top {{ Date.now() }}</h1>
+  <MiddleComponent />
+  <button @click="switchColor">Switch (current: {{ color }})</button>
+</template>
+
+<style scoped></style>
