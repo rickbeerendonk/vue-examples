72551f22e9bff1dc5af5cb2df559087238ffd482
Rick Beerendonk <rick@beerendonk.com>
2025-03-26 10:45:44 +0100

    Add "Slots - useSlots" example.

---
+++ 10 - Slots/f. useSlots/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,23 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2025 Rick Beerendonk          -->
+
+<script setup>
+import TabContainer from './TabContainer.vue';
+</script>
+
+<template>
+  <TabContainer>
+    <template #first>
+      <em>First</em>
+    </template>
+
+    <template #second>
+      <b>Second</b>
+    </template>
+    <template #third>
+      <u>Third</u>
+    </template>
+  </TabContainer>
+</template>
+
+<style scoped></style>
---
+++ 10 - Slots/f. useSlots/composition-api/compiled/js/src/TabContainer.vue
@@ -0,0 +1,31 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2025 Rick Beerendonk          -->
+
+<script setup>
+import { ref, useSlots } from 'vue';
+
+const slots = useSlots();
+const tabs = ref(Object.keys(slots));
+const selectedTab = ref(tabs.value[0]);
+
+const setSelectTab = tab => {
+  selectedTab.value = tab;
+};
+</script>
+
+<template>
+  <button
+    v-for="tab in tabs"
+    :key="tab"
+    :disabled="tab === selectedTab"
+    @click="setSelectTab(tab)"
+  >
+    {{ tab }}
+  </button>
+
+  <div>
+    <slot :name="selectedTab" />
+  </div>
+</template>
+
+<style scoped></style>
