b2759c23e51e8f9947051bff0670ff8446168df2
Rick Beerendonk <rick@beerendonk.com>
2025-03-27 00:45:33 +0100

    Add compiled version.

---
+++ 29 - Suspense/1. Async Components/composition-api/compiled/js/src/App.vue
@@ -0,0 +1,27 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
+<script setup>
+import { ref, defineAsyncComponent } from 'vue';
+
+const Greeting = defineAsyncComponent(() => import('./Greeting.vue'));
+const visible = ref(false);
+</script>
+
+<template>
+  <button @click="visible = !visible">
+    {{ visible ? 'Hide' : 'Show' }}
+  </button>
+
+  <Suspense v-if="visible">
+    <template #default>
+      <Greeting />
+    </template>
+
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
+</template>
+
+<style scoped></style>
---
+++ 29 - Suspense/1. Async Components/composition-api/compiled/js/src/Greeting.vue
@@ -0,0 +1,10 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
+<script setup></script>
+
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<style scoped></style>
