6b453ca5a09ebe924283c13fd9794b867d590ddc
Rick Beerendonk <rick@beerendonk.com>
2025-03-27 15:26:58 +0100

    Add "Nuxt - Fetch" demo.

---
+++ Nuxt/5. Fetch (server and client)/pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Posts</h2>
+    <nav>
+      <ol>
+        <li v-for="post in posts" :key="post.id" :value="post.id">
+          <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
+        </li>
+      </ol>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+// Fetch data both server-side and client-side, rename the data to posts
+const { data: posts } = await useFetch(
+  'https://jsonplaceholder.typicode.com/posts'
+);
+</script>
