4eaaf6a1bdab01c8ee5814985f97c8f35454fd28
Rick Beerendonk <rick@beerendonk.com>
2025-03-27 10:51:36 +0100

    Add composition api version.

---
+++ Router/10 - Navigation Guards/c. In-Component/composition-api/compiled/js/src/views/Page2View.vue
@@ -0,0 +1,21 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
+<script setup>
+import { onBeforeRouteLeave } from 'vue-router';
+
+// Use the `onBeforeRouteLeave` hook to define the guard
+onBeforeRouteLeave((to, from, next) => {
+  if (confirm('Do you want to leave?')) {
+    next(); // Allow navigation
+  } else {
+    next(false); // Cancel navigation
+  }
+});
+</script>
+
+<template>
+  <h2>Page 2</h2>
+</template>
+
+<style scoped></style>
