1b10c7b0a909532c289e93671beeed8f2da3137b
Rick Beerendonk <rick@beerendonk.com>
2025-03-27 09:43:37 +0100

    Add composition api version.

---
+++ Router/08 - Navigation using Code/composition-api/compiled/js/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+function goToPage1() {
+  router.push({ path: '/page1' });
+}
+</script>
+
+<template>
+  <h2>Home</h2>
+
+  <button @click="goToPage1">Page 1</button>
+</template>
+
+<style scoped></style>
