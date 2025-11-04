382fe7a3f7d51fd8ab13ccf0043443f100fd5352
Rick Beerendonk <rick@beerendonk.com>
2025-11-04 16:28:30 +0100

    Add TypeScript version.

---
+++ 08 - Component Interaction/2. Properties/c. Default Values/composition-api/compiled/ts/src/components/GreetingComponent.vue
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<script setup lang="ts">
+interface Props {
+  name?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  name: 'World'
+});
+</script>
+
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<style scoped></style>
