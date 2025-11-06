53fe346732771530fabe2773bd6e28c3fe90f121
Rick Beerendonk <rick@beerendonk.com>
2025-11-06 03:01:00 +0100

    Add "Compound Components - Default" example.

---
+++ 31 - Compound Components/c. Default/compiled/ts/src/App.vue
@@ -0,0 +1,11 @@
+<!--! European Union Public License version 1.2 !-->
+<!--! Copyright © 2025 Rick Beerendonk          !-->
+
+<script setup lang="ts">
+import GreetingEditor from './components/GreetingEditor.vue';
+</script>
+
+<template>
+  <!-- No children provided - will use default children -->
+  <GreetingEditor />
+</template>
---
+++ 31 - Compound Components/c. Default/compiled/ts/src/main.ts
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
