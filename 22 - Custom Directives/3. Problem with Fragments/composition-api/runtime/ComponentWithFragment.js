b2e434aab3dedf699abcd4a251fb78d374c23ff1
Rick Beerendonk <rick@beerendonk.com>
2025-11-06 10:35:35 +0100

    Add Component API version.

---
+++ 22 - Custom Directives/3. Problem with Fragments/composition-api/runtime/ComponentWithFragment.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+export default {
+  name: 'ComponentWithFragment',
+  template: `
+    <h1>Component with Fragment</h1>
+    <p>Paragraph in Fragment</p>
+  `
+};
